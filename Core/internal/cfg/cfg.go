1e11be24350c20ffae48bc5ace44a59679824d80
unk_xui <xbydji@gmail.com>
2022-11-06 21:27:21 +0400

    Add main files and env variables

--- Core/internal/cfg/cfg.go
+++ Core/internal/cfg/cfg.go
@@ -1,1 +1,74 @@
 package cfg
+
+import (
+	"core/pkg/logger"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type Config struct {
+	Port string
+	IP   string
+
+	DbName     string
+	DbPassword string
+	DbUser     string
+	DbPort     string
+}
+
+var Cfg Config
+
+func LoadEnv() {
+	log := logger.GetLogger()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Errorf("Error load env ! %s", err)
+		return
+	}
+	log.Info("ENV loaded ! ")
+}
+
+func InitCfg() {
+	log := logger.GetLogger()
+
+	port, exst := os.LookupEnv("PORT")
+	if !exst {
+		log.Errorf("Error get PORT")
+	}
+
+	id, exst := os.LookupEnv("ID")
+	if !exst {
+		log.Errorf("Error get ID")
+	}
+
+	dbName, exst := os.LookupEnv("DBNAME")
+	if !exst {
+		log.Errorf("Error get DbName")
+	}
+
+	dbPassword, exst := os.LookupEnv("DBPASSWORD")
+	if !exst {
+		log.Errorf("Error get DBPASSWORD")
+	}
+
+	dbPort, exst := os.LookupEnv("DBPORT")
+	if !exst {
+		log.Errorf("Error get DBPORT")
+	}
+
+	dbUser, exst := os.LookupEnv("DBUSER")
+	if !exst {
+		log.Errorf("Error get DBUSER")
+	}
+
+	Cfg = Config{
+		Port: port,
+		IP:   id,
+
+		DbName:     dbName,
+		DbPassword: dbPassword,
+		DbPort:     dbPort,
+		DbUser:     dbUser,
+	}
+}
---
+++ Core/pkg/db/db.go
@@ -0,0 +1,118 @@
+package db
+
+import (
+	"core/internal/cfg"
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type UserInfo struct {
+	Id           uint32 `json:"id" gorm:"primaryKey"`
+	Phone        string `json:"phone"`
+	Token        string `json:"jwt"`
+	RefreshToken string `json:"refresh_jwt"`
+}
+
+var DataBase *gorm.DB
+
+func InitDb() {
+	dbURL := fmt.Sprintf("postgres://%s:%s@localhost:%s/%s", cfg.Cfg.DbUser, cfg.Cfg.DbPassword, cfg.Cfg.DbPort, cfg.Cfg.DbName)
+
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	if err != nil {
+		fmt.Printf("Error initialize DB %s \n", err.Error())
+		return
+	}
+	fmt.Println("DB initialize!")
+	DataBase = db
+}
+
+func AutoMigrate() error {
+	err := DataBase.AutoMigrate(&UserInfo{})
+	if err != nil {
+		fmt.Printf("Error migrate DB %s \n", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func AddUser(user UserInfo) error {
+	ok := DataBase.Create(&user)
+	if ok.Error != nil {
+		fmt.Printf("Error AddUser %s \n", ok.Error.Error())
+		return ok.Error
+	}
+
+	fmt.Printf("User added to database %s \n", user.Phone)
+	return nil
+}
+
+func DeleteUser(id uint64) error {
+	user := UserInfo{}
+	ok := DataBase.Find(&user, id)
+	if ok.Error != nil {
+		fmt.Printf("Error AddUser %s \n", ok.Error.Error())
+		return ok.Error
+	}
+
+	DataBase.Delete(&user)
+
+	return nil
+}
+
+func GetUser(id uint32) (UserInfo, error) {
+	user := UserInfo{}
+	ok := DataBase.Find(&user, id)
+	if ok.Error != nil {
+		fmt.Printf("Error AddUser %s \n", ok.Error.Error())
+		return user, ok.Error
+	}
+
+	return user, nil
+}
+
+func GetAllUsers() ([]UserInfo, error) {
+	var users []UserInfo
+	ok := DataBase.Find(&users)
+	if ok.Error != nil {
+		fmt.Printf("Error AddUser %s \n", ok.Error.Error())
+		return nil, ok.Error
+	}
+
+	return users, nil
+}
+
+func GetUserByPhone(phone string) UserInfo {
+	ret := UserInfo{}
+	users, err := GetAllUsers()
+	if err != nil {
+		fmt.Printf("Error GetAllUsers in GetUserByEmail \n")
+		return ret
+	}
+
+	for _, v := range users {
+		if v.Phone == phone {
+			return v
+		}
+	}
+
+	return ret
+}
+
+func UpdateUser(user UserInfo) error {
+	userBase := UserInfo{}
+	ok := DataBase.Find(&userBase, user.Id)
+	if ok.Error != nil {
+		fmt.Printf("Error AddUser %s \n", ok.Error.Error())
+		return ok.Error
+	}
+
+	userBase.Phone = user.Phone
+	userBase.Token = user.Token
+
+	DataBase.Save(&userBase)
+
+	return nil
+}
